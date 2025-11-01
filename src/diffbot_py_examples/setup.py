5a7fa6d0ab7c738103a7af12540704aed96ee389
Adnan Alvi <adnanalvi17092001@gmail.com>
2025-11-01 19:56:30 +0530

    Added python simple_subscriber Node

--- src/diffbot_py_examples/setup.py
+++ src/diffbot_py_examples/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "simple_publisher = diffbot_py_examples.simple_publisher:main"
+            "simple_publisher = diffbot_py_examples.simple_publisher:main",
+            "simple_subscriber = diffbot_py_examples.simple_subscriber:main"
         ],
     },
 )
